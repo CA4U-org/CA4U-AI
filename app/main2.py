5e376c105f6fe18497acec32842408c8c8642c1e
박지후 <jihoo33@naver.com>
2024-11-05 00:52:51 +0900

    Update main2.py

--- app/main2.py
+++ app/main2.py
@@ -13,22 +13,26 @@ from recommend_clubs import recommend_clubs
 app = FastAPI()
 
 
-model_data = None
+recommend_model = None
 
 
-@app.get("/")
-def root():
-    global model_data
-    df = fetcher()
-    adapted_data = adapter(df)
-    final_similarity, data = analysis(preprocess(adapted_data))
-    model_data = final_similarity, data  
+@app.get("/clubs/recommend/create")
+def create_recommend_model():
+    global recommend_model
+    if recommend_model is None:  
+        df = fetcher()
+        adapted_data = adapter(df)
+        final_similarity, data = analysis(preprocess(adapted_data))
+        recommend_model = final_similarity, data
+        return {"message": "추천 모델이 성공적으로 생성되었습니다."}
+    else:
+        return {"message": "추천 모델이 이미 생성되어 있습니다."}
 
-@app.get("/model-use")
-def model_use():
-    global model_data
+# 추천 시스템 엔드포인트
+@app.get("/clubs/recommend")
+def get_recommendations():
+    global recommend_model
     favorites = '피카통' 
-    final_similarity, data = model_data
+    final_similarity, data = recommend_model
     recommended_clubs = recommend_clubs(favorites, final_similarity, data)
     return {"recommended_club": recommended_clubs}
-
