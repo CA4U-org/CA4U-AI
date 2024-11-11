9f312b90129cbe9ebf28306ac0bb4144ceae11a4
박지후 <jihoo33@naver.com>
2024-11-11 14:17:15 +0900

    update preprocess.py

--- app/preprocess.py
+++ app/preprocess.py
@@ -1,9 +1,10 @@
 # Preprocess 함수
 def preprocess(data):
     stopwords = ['중앙대', '중앙대학교', '동아리', 'PM', '명', '기수','1학기', '00', '0', '정기모임', '기준', '약', '2024', '부원', '여',
-        '활동', '중앙', '모집', '학기', '학회', '대학교', '합니다', '입니다', '및','있습니다', '지원', '기간', ',', '.', '-', '(', ')', '!', ':','?','~']
+        '활동', '중앙', '모집', '학기', '학회', '대학교', '합니다', '입니다', '및','있습니다', '지원', '기간', ',', '.', '-', '!', ':','?','~']
     
     import os
+    import re
     def preprocess_stopwords_file():
         # 현재 파일 위치를 기준으로 stopword.txt 파일 경로 설정
         base_dir = os.path.dirname(__file__)
@@ -15,14 +16,15 @@ def preprocess(data):
     
     stopwords += preprocess_stopwords_file()
      
-    data = data.fillna(0)
+    data = data.fillna(0) #결측치 제거
     
     def preprocess_text(text):
         text = str(text)
-        text = text.replace("\n", " ").replace("\n\n", " ")  
-        text = ' '.join(text.split()) 
-        for stopword in stopwords:
-            text = text.replace(stopword, '')
+        text = text.replace("\n", " ").replace("\n\n", " ")  # 줄바꿈 제거
+        text = ' '.join(text.split())  # 중복 공백 제거
+        # 단어 단위로 stopwords 제거
+        pattern = r'\b(' + '|'.join(map(re.escape, stopwords)) + r')\b'
+        text = re.sub(pattern, '', text)
         return text
     
     for col in data.columns[1:]:
