30fb99646a3a9b3d48e6ef8f6a5ef2254d5baacd
seobeomseok <sbslc2000@gmail.com>
2024-11-12 17:37:30 +0900

    chore: add docs on DB

--- app/db/db.py
+++ app/db/db.py
@@ -20,10 +20,39 @@ class DB:
         finally:
             self.pool.return_connection(connection)
 
+    """
+        * findAll(table)
+        * table에 해당하는 데이터 목록 전체를 조회합니다.
+        * 조회한 결과는 dictionary로 반환됩니다.
+        *
+        * Ex.
+        * from core.ctx import CTX
+        *
+        * data = CTX.DB.findAll("USER") 
+    """
     def findAll(self, table):
         query = f"SELECT * FROM {table}"
         return self.execute_with_connection(query)
+    
 
+    """
+        * findByCreatedAtBetween(table, start, end)
+        * 특정 기간 내에 생성된 데이터의 목록을 조회합니다. 
+        * start와 end는 datetime을 사용합니다. 
+        *
+        * Ex.
+        * from datetime import datetime
+        * from core.ctx import CTX
+        *
+        * start_date = datetime(2024,1,1)
+        * end_date = datetime(2024,12,31,23,59,59)
+        *
+        * data = CTX.DB.findByCreatedAtBetween(
+        *   "USER", start_date, end_date
+        * ) 
+        *
+        * 조회한 결과는 dictionary로 반환됩니다.
+    """
     def findByCreatedAtBetween(self, table, start, end):
         query = f"SELECT * FROM {table} WHERE created_at > %s AND created_at < %s"
         return self.execute_with_connection(query, (start, end))
