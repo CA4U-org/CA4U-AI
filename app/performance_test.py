9a32a9c7b285a3f246bd8645726f888aec98cf9a
우단비 <dneksql0314@gmail.com>
2025-01-06 00:41:27 +0900

    성능평가

--- app/performance_test.py
+++ app/performance_test.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 
 
-#### 1. 동아리 데이터
+
+########################### 1. 동아리 데이터 ########################
 
 # 프로젝트 루트 기준 상대 경로 설정
 base_dir = os.path.dirname(__file__)
@@ -14,7 +15,9 @@ club_df.set_index('ID', inplace=True)
 print(club_df)
 
 
-#### 2. 사용자 데이터 생성
+
+########################## 2. 사용자 데이터 생성 ##########################
+
 club_names = club_df.index
 
 num_users = 100
@@ -24,7 +27,6 @@ data = np.random.randint(0, 2, size=(num_users, len(club_names)))
 # 사용자 df 생성
 users = [f'user_{i+1}' for i in range(num_users)]
 user_df = pd.DataFrame(data, index=users, columns=club_names)
-
 print(user_df)
 
 # 사용자별로 리스트 생성
@@ -37,8 +39,9 @@ for user in user_df.index:
 for user, ids in user_id_lists.items():
     print(ids)
  
+
     
-#### 3. 내적 계산
+########################## 3. 내적 계산 ###########################
 user_category_matrix = user_df.dot(club_df)
 
 # normalization
@@ -46,7 +49,8 @@ user_category_norm = user_category_matrix.div(user_category_matrix.sum(axis=1), 
 print(user_category_norm)
 
 
-#### 4. 사용자 선호 판단
+
+######################### 4. 사용자 선호 판단 #######################
 
 result = user_category_norm.reset_index().melt(
     id_vars='index',
@@ -79,7 +83,9 @@ user_favorites = get_user_favorites(result)
 print(user_favorites)
 
 
-#### 5. 알고리즘 설계
+
+############################# 5. 알고리즘 설계 ###########################
+
 content_recommend_model = None
 
 from main import initialize_content_model
@@ -108,12 +114,15 @@ def get_recommendations(clubIDs: str, top_n: int = 3):
     selected_ids = [int(id.strip()) for id in clubIDs.split(",")] # clubIDs를 쉼표로 구분하여 리스트로 변환
     return {"recommended_clubs": content_recommend_clubs_n(selected_ids, final_similarity, final_data, top_n=top_n)}
 
-# Example
-recommendation_result = get_recommendations("2,6,10,15,17")
+# Example (User 1)
+recommendation_result = get_recommendations("2,6,10,15,17") 
 print(recommendation_result)
 
 
-#### 6. 성능평가
+
+
+############################# 6. 성능평가 ###############################
+
 # user1의 데이터만 필터링
 user1_favorites = user_favorites[user_favorites['user'] == 'user_1']
 print(user1_favorites)
