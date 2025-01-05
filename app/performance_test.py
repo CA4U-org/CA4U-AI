e3418436c12047ee148a532907a10c93e6b7246a
박지후 <144203639+jihoo420@users.noreply.github.com>
2025-01-06 01:32:02 +0900

    Update performance_test.py

--- app/performance_test.py
+++ app/performance_test.py
@@ -90,6 +90,45 @@ content_recommend_model = None
 
 from main import initialize_content_model
 
+def analysis_test(data):
+    
+    tfidf_matrix = {}
+    for col in data.columns[1:]:
+        vectorizer = TfidfVectorizer(token_pattern=r"(?u)\b\w+\b")
+        tfidf_matrix[col] = vectorizer.fit_transform(data[col])
+
+    cosine_sim = {}
+    for col in data.columns[1:]:
+        cosine_sim[col] = cosine_similarity(tfidf_matrix[col])
+
+    weights = {
+        'id':0,
+        'club_nm': 0,
+        'category_id':0.5,
+        'brief_description': 0.5,
+        'recruit_description': 0.5,
+        'target_people_description': 0.3,
+        'target_cycle_description': 0.1,
+        'apply_description': 0.3,
+        'act_day_description': 0.3,
+        'location_description': 0.1,
+        'cost_description': 0.1,
+        'spec_description': 0.5
+    }
+
+    final_similarity = np.zeros_like(cosine_sim[data.columns[1]])
+    for col in data.columns[1:]:
+        final_similarity += weights[col] * cosine_sim[col]
+    
+    return final_similarity, data
+
+def initialize_content_model():
+    fetcher_df = club_fetcher()
+    adapted_df = content_adapter(fetcher_df)
+    preprocess_df = preprocess(adapted_df)
+    final_similarity, final_data = analysis_test(preprocess_df) 
+    return final_similarity, final_data 
+    
 def content_recommend_clubs_n(selected_ids, final_similarity, data, top_n=3):
     aggregated_scores = {}
     for selected_id in selected_ids:
